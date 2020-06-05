234e042acad0fdb90a7e1934c46b3d540ede089a
D_MENT <rocket111185@gmail.com>
2020-06-05 15:55:30 +0300

    stylechecking.sh: changed search mask, added comments on process

--- stylechecking.sh
+++ stylechecking.sh
@@ -5,10 +5,14 @@
 # SC2038 - in order to avoid non-alphanumeric filenames.
 # SC2059 - in order to allow using variables in the printf format string.
 
+# First, check if `shellcheck` is installed.
+# Then, find all the executable files (omitting .git directory)
+# and check them for tne styles (excluding disabled).
 
 if [ "$(command -v shellcheck)" ];
 then
-    find . -type f -executable ! -name '*.sample' | xargs -I{} shellcheck {} -e SC2038 -e SC2059;
+    find . -type f -executable -not -path "./.git/*" | xargs -I{} shellcheck {} -e SC2038 -e SC2059;
 else
     printf "Install shellcheck first.\\n";
+    exit 1;
 fi
