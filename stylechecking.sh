33a9844e27bc78b99b317b504f5bd3c6fd77eda0
D_MENT <rocket111185@gmail.com>
2020-06-05 08:19:14 +0300

    stylechecking.sh: comments on disabled rules

--- stylechecking.sh
+++ stylechecking.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Disabled rules:
+#
+# SC2038 - in order to avoid non-alphanumeric filenames.
+# SC2059 - in order to allow using variables in the printf format string.
+
+
 if [ "$(command -v shellcheck)" ];
 then
     find . -type f -executable ! -name '*.sample' | xargs -I{} shellcheck {} -e SC2038 -e SC2059;
